f606e34f4603f9e0b55e30b10c0bc5fc4a612630
Rodrigo Assis <rodrigo.assis@xlsol.com>
2013-11-27 19:35:00 -0200

    separate omniauth hashes in methods

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -36,11 +36,7 @@ describe RegistrationsController do
   end
 
   it "verifies google authentication for a new user" do
-    omniauth_hash = { provider: "google_oauth2",
-                      uid: "123545",
-                      info: {name: "John Doe", email: "johndoe@email.com"},
-                      credentials: {token: "testtoken234tsdf"}
-                    }
+    omniauth_hash = google_omniauth_hash
 
     request.env["omniauth.auth"] = OmniAuth.config.add_mock(:google_oauth2, omniauth_hash)
 
@@ -57,11 +53,7 @@ describe RegistrationsController do
   it "verifies google authentication for already registered user" do
     user = FactoryGirl.create(:user, name: "John Doe", email: "johndoe@email.com", password: "password")
 
-    omniauth_hash = { provider: "google_oauth2",
-                      uid: "123545",
-                      info: {name: "John Doe", email: "johndoe@email.com"},
-                      credentials: {token: "testtoken234tsdf"}
-                    }
+    omniauth_hash = google_omniauth_hash
 
     request.env["omniauth.auth"] = OmniAuth.config.add_mock(:google_oauth2, omniauth_hash)
 
@@ -75,15 +67,7 @@ describe RegistrationsController do
   end
 
   it "verifies facebook authentication for a new user" do
-    omniauth_hash = { "provider"=>"facebook",
-                      "uid"=>"123123123123",
-                      "info"=> {
-                        "email"=>"johndoe@email.com", "name"=>"John Doe"
-                      },
-                      "extra"=> {
-                        "raw_info"=>{"name"=>"John Doe"}
-                      }
-                    }
+    omniauth_hash = facebook_omniauth_hash
 
     request.env["omniauth.auth"] = OmniAuth.config.add_mock(:facebook, omniauth_hash)
 
@@ -100,15 +84,7 @@ describe RegistrationsController do
   it "verifies facebook authentication for already registered user" do
     user = FactoryGirl.create(:user, name: "John Doe", email: "johndoe@email.com", password: "password")
 
-    omniauth_hash = { "provider"=>"facebook",
-                      "uid"=>"123123123123",
-                      "info"=> {
-                        "email"=>"johndoe@email.com", "name"=>"John Doe"
-                      },
-                      "extra"=> {
-                        "raw_info"=>{"name"=>"John Doe"}
-                      }
-                    }
+    omniauth_hash = facebook_omniauth_hash
 
     request.env["omniauth.auth"] = OmniAuth.config.add_mock(:facebook, omniauth_hash)
 
@@ -122,66 +98,7 @@ describe RegistrationsController do
   end
 
   it "verifies twitter authentication for a new user" do
-    omniauth_hash = {
-                      :provider => "twitter",
-                      :uid => "123456",
-                      :info => {
-                        :nickname => "john_doe",
-                        :name => "John Doe",
-                        :description => "a very normal guy.",
-                        :urls => {
-                          :Website => nil,
-                          :Twitter => "https://twitter.com/john_doe"
-                        }
-                      },
-                       :extra => {
-                        :access_token => "", # An OAuth::AccessToken object
-                        :raw_info => {
-                          :name => "John Q Public",
-                          :listed_count => 0,
-                          :profile_sidebar_border_color => "181A1E",
-                          :url => nil,
-                          :lang => "en",
-                          :statuses_count => 129,
-                          :profile_image_url => "http://si0.twimg.com/sticky/default_profile_images/default_profile_2_normal.png",
-                          :profile_background_image_url_https => "https://twimg0-a.akamaihd.net/profile_background_images/229171796/pattern_036.gif",
-                          :location => "Anytown, USA",
-                          :time_zone => "Chicago",
-                          :follow_request_sent => false,
-                          :id => 123456,
-                          :profile_background_tile => true,
-                          :profile_sidebar_fill_color => "666666",
-                          :followers_count => 1,
-                          :default_profile_image => false,
-                          :screen_name => "",
-                          :following => false,
-                          :utc_offset => -3600,
-                          :verified => false,
-                          :favourites_count => 0,
-                          :profile_background_color => "1A1B1F",
-                          :is_translator => false,
-                          :friends_count => 1,
-                          :notifications => false,
-                          :geo_enabled => true,
-                          :profile_background_image_url => "http://twimg0-a.akamaihd.net/profile_background_images/229171796/pattern_036.gif",
-                          :protected => false,
-                          :description => "a very normal guy.",
-                          :profile_link_color => "2FC2EF",
-                          :created_at => "Thu Jul 4 00:00:00 +0000 2013",
-                          :id_str => "123456",
-                          :profile_image_url_https => "https://si0.twimg.com/sticky/default_profile_images/default_profile_2_normal.png",
-                          :default_profile => false,
-                          :profile_use_background_image => false,
-                          :entities => {
-                            :description => {
-                              :urls => []
-                            }
-                          },
-                          :profile_text_color => "666666",
-                          :contributors_enabled => false
-                        }
-                      }
-                    }
+    omniauth_hash = twitter_omniauth_hash
 
     request.env["omniauth.auth"] = OmniAuth.config.add_mock(:twitter, omniauth_hash)
 
@@ -198,66 +115,7 @@ describe RegistrationsController do
   it "verifies twitter authentication for already registered user" do
     user = FactoryGirl.create(:user, name: "John Doe", email: "john_doe@fake_twitter_email.com", password: "password")
 
-    omniauth_hash = {
-                      :provider => "twitter",
-                      :uid => "123456",
-                      :info => {
-                        :nickname => "john_doe",
-                        :name => "John Doe",
-                        :description => "a very normal guy.",
-                        :urls => {
-                          :Website => nil,
-                          :Twitter => "https://twitter.com/john_doe"
-                        }
-                      },
-                       :extra => {
-                        :access_token => "", # An OAuth::AccessToken object
-                        :raw_info => {
-                          :name => "John Q Public",
-                          :listed_count => 0,
-                          :profile_sidebar_border_color => "181A1E",
-                          :url => nil,
-                          :lang => "en",
-                          :statuses_count => 129,
-                          :profile_image_url => "http://si0.twimg.com/sticky/default_profile_images/default_profile_2_normal.png",
-                          :profile_background_image_url_https => "https://twimg0-a.akamaihd.net/profile_background_images/229171796/pattern_036.gif",
-                          :location => "Anytown, USA",
-                          :time_zone => "Chicago",
-                          :follow_request_sent => false,
-                          :id => 123456,
-                          :profile_background_tile => true,
-                          :profile_sidebar_fill_color => "666666",
-                          :followers_count => 1,
-                          :default_profile_image => false,
-                          :screen_name => "",
-                          :following => false,
-                          :utc_offset => -3600,
-                          :verified => false,
-                          :favourites_count => 0,
-                          :profile_background_color => "1A1B1F",
-                          :is_translator => false,
-                          :friends_count => 1,
-                          :notifications => false,
-                          :geo_enabled => true,
-                          :profile_background_image_url => "http://twimg0-a.akamaihd.net/profile_background_images/229171796/pattern_036.gif",
-                          :protected => false,
-                          :description => "a very normal guy.",
-                          :profile_link_color => "2FC2EF",
-                          :created_at => "Thu Jul 4 00:00:00 +0000 2013",
-                          :id_str => "123456",
-                          :profile_image_url_https => "https://si0.twimg.com/sticky/default_profile_images/default_profile_2_normal.png",
-                          :default_profile => false,
-                          :profile_use_background_image => false,
-                          :entities => {
-                            :description => {
-                              :urls => []
-                            }
-                          },
-                          :profile_text_color => "666666",
-                          :contributors_enabled => false
-                        }
-                      }
-                    }
+    omniauth_hash = twitter_omniauth_hash
 
     request.env["omniauth.auth"] = OmniAuth.config.add_mock(:twitter, omniauth_hash)
 
@@ -270,4 +128,87 @@ describe RegistrationsController do
     expect(user.email).to eq "john_doe@fake-twitter-email.com"
   end
 
+end
+
+def google_omniauth_hash
+  { provider: "google_oauth2",
+    uid: "123545",
+    info: {name: "John Doe", email: "johndoe@email.com"},
+    credentials: {token: "testtoken234tsdf"}
+  }
+end
+
+def facebook_omniauth_hash
+  { "provider"=>"facebook",
+    "uid"=>"123123123123",
+    "info"=> {
+      "email"=>"johndoe@email.com", "name"=>"John Doe"
+    },
+    "extra"=> {
+      "raw_info"=>{"name"=>"John Doe"}
+    }
+  }
+end
+
+def twitter_omniauth_hash
+  {
+    :provider => "twitter",
+    :uid => "123456",
+    :info => {
+      :nickname => "john_doe",
+      :name => "John Doe",
+      :description => "a very normal guy.",
+      :urls => {
+        :Website => nil,
+        :Twitter => "https://twitter.com/john_doe"
+      }
+    },
+     :extra => {
+      :access_token => "", # An OAuth::AccessToken object
+      :raw_info => {
+        :name => "John Q Public",
+        :listed_count => 0,
+        :profile_sidebar_border_color => "181A1E",
+        :url => nil,
+        :lang => "en",
+        :statuses_count => 129,
+        :profile_image_url => "http://si0.twimg.com/sticky/default_profile_images/default_profile_2_normal.png",
+        :profile_background_image_url_https => "https://twimg0-a.akamaihd.net/profile_background_images/229171796/pattern_036.gif",
+        :location => "Anytown, USA",
+        :time_zone => "Chicago",
+        :follow_request_sent => false,
+        :id => 123456,
+        :profile_background_tile => true,
+        :profile_sidebar_fill_color => "666666",
+        :followers_count => 1,
+        :default_profile_image => false,
+        :screen_name => "",
+        :following => false,
+        :utc_offset => -3600,
+        :verified => false,
+        :favourites_count => 0,
+        :profile_background_color => "1A1B1F",
+        :is_translator => false,
+        :friends_count => 1,
+        :notifications => false,
+        :geo_enabled => true,
+        :profile_background_image_url => "http://twimg0-a.akamaihd.net/profile_background_images/229171796/pattern_036.gif",
+        :protected => false,
+        :description => "a very normal guy.",
+        :profile_link_color => "2FC2EF",
+        :created_at => "Thu Jul 4 00:00:00 +0000 2013",
+        :id_str => "123456",
+        :profile_image_url_https => "https://si0.twimg.com/sticky/default_profile_images/default_profile_2_normal.png",
+        :default_profile => false,
+        :profile_use_background_image => false,
+        :entities => {
+          :description => {
+            :urls => []
+          }
+        },
+        :profile_text_color => "666666",
+        :contributors_enabled => false
+      }
+    }
+  }
 end
