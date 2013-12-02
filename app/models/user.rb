78db030a35ec3b702952d298deed6a865414f4fe
Rodrigo Assis <rodrigo.assis@xlsol.com>
2013-12-01 23:07:20 -0200

    script

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,13 +28,13 @@ class User < ActiveRecord::Base
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
     # Find the user from provider or try to match email
     user = User.where(:provider => auth.provider, :uid => auth.uid).first || User.where(email: auth.info.email).first
-    
+
     # Didn't find any user so create one
     unless user
-      user = User.create(name: auth.extra.raw_info.name, provider: auth.provider, uid: auth.uid, 
+      user = User.create(name: auth.extra.raw_info.name, provider: auth.provider, uid: auth.uid,
                           email: auth.info.email, password: Devise.friendly_token[0,20])
     end
-    
+
     return user
   end
 
@@ -42,13 +42,13 @@ class User < ActiveRecord::Base
   def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
     # Find the user from provider or try to match email
     user = User.where(:provider => auth.provider, :uid => auth.uid).first
-    
+
     # Didn't find any user so create one
     unless user
-      user = User.create(name: auth.info.name, provider: auth.provider, uid: auth.uid, 
+      user = User.create(name: auth.info.name, provider: auth.provider, uid: auth.uid,
                           email: "#{auth.info.nickname}@fake-twitter-email.com", password: Devise.friendly_token[0,20])
     end
-    
+
     return user
   end
 end
