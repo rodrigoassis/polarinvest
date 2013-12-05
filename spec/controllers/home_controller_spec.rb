097d56709b392b3da07984805e4a39ea49171095
Júlio Bueno <julio@codepolaris.com>
2013-12-05 15:32:23 -0200

    Added tests for dashboard and search methods. Not quite good but it's something.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
 
 describe HomeController do
+  render_views
+
+  def sign_in_user provider=nil
+    user = FactoryGirl.create(:user, provider: provider)
+    visit "/users/sign_in"
+    fill_in "Email",                 :with => user.email
+    fill_in "Password",              :with => user.password
+    click_button "Sign in"
+    return user
+  end
+
   it "don't allows users to sign an account with any form data" do
     visit "/users/sign_up"
 
@@ -39,6 +50,36 @@ describe HomeController do
 
     click_button "Sign up"
 
+    current_path.should == "/"
+  end
+
+  it "allows user to access dashboard" do
+    user = sign_in_user
+    visit "/dashboard"
+
+    current_path.should == "/dashboard"
+  end
+
+  it "allows signed user to access dashboard" do
+    user = sign_in_user
+    visit "/dashboard"
+
+    current_path.should == "/dashboard"
+  end
+
+  it "don't allows user to access dashboard without sign_in" do
+    visit "/dashboard"
+
+    current_path.should == "/users/sign_in"
+  end
+
+  it "can retrieve asset from search" do
+    asset = FactoryGirl.create(:asset)
+    visit "/"
+
+    fill_in "Asset", with: "Poupança"
+    click_button "Search"
+    
     current_path.should == "/"
   end
 
