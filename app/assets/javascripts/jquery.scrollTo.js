347bcbe523ea735f5a7e4beba02eb7cbd91ca50a
Rodrigo Assis <rodrigo.assis@xlsol.com>
2013-12-06 20:17:37 -0200

    minor refactoring

--- app/assets/javascripts/jquery.scrollTo.js
+++ app/assets/javascripts/jquery.scrollTo.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $('a.anchor').click(function(event) {
     var id = $(this).attr("href");
     var height = $("#nav-bar").height();
-    var target = $(id).offset().top - height;
+    var target = $(id).offset().top - height + 1;
 
     $('html, body').animate({scrollTop:target}, 500);
     event.preventDefault();
