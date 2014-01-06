e55035ac31a3d5c70358aa1479488c27c7212f82
Marcos Serpa <marcosserpa@gmail.com>
2014-01-06 21:05:28 -0200

    Partially testet business class 'bovespa'.

--- spec/models/business/bovespa_spec.rb
+++ spec/models/business/bovespa_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe "Bovespa" do
-	# # Class method self.extract_codes
-	# it "returns an array of codes" do
-	# 	expect(Bovespa.extract_codes).to_not eq nil
-	# end
+	# Class method self.extract_codes
+	it "returns an array of codes" do
+		expect(Business::Bovespa.extract_codes).to_not eq nil
+	end
 
-	# # Class method self.extract_codes
-	# it "returns an array of codes" do
-	# 	expect(Bovespa.extract_codes).to_not eq ""
-	# end
+	# Class method self.extract_codes
+	it "returns an array of codes" do
+		expect(Business::Bovespa.extract_codes).to_not eq ""
+	end
 end
