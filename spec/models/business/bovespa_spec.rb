2078bc06ac94eae11a1b76091a7bfe5785044378
Marcos Serpa <marcosserpa@gmail.com>
2014-01-08 16:02:03 -0200

    Tests.

--- spec/models/business/bovespa_spec.rb
+++ spec/models/business/bovespa_spec.rb
@@ -10,4 +10,30 @@ describe "Bovespa" do
 	it "returns an array of codes" do
 		expect(Business::Bovespa.extract_codes).to_not eq ""
 	end
+
+  it "returns an array of market situation" do
+    expect(Business::Bovespa.fetch_codes).to_not eq ""
+  end
+
+  it "returns an array of market situation" do
+    expect(Business::Bovespa.fetch_codes).to_not eq nil
+  end
+
+  it "find a ticker" do
+    AssetTypes::Share.create(ticker: "PETR4", name: "Petrobras")
+
+    expect(Business::Bovespa.find_share("PETR4", "")).to be_true
+  end
+
+  it "create a ticker" do
+    expect(Business::Bovespa.find_share("PETR4", "")).to be_true
+  end
+
+  it "create a historical value" do
+    AssetTypes::Share.create(ticker: "PETR4", name: "Petrobras")
+    asset = Asset.all.first
+    line = "00COTAHIST.2012BOVESPA 20121228                                                                                                                                                                                                                      \r\n"
+
+    expect(Business::Bovespa.save_history_values(line, asset)).to be_true
+  end
 end
