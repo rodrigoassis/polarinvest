d3dbf18c39dbead617f3b1575bf53e246b4e260f
Júlio Bueno <julioce@gmail.com>
2014-02-11 15:42:21 -0200

    Changed login from proviver and uid key to email only

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,13 +14,12 @@ class User < ActiveRecord::Base
 
   def self.find_for_oauth(auth)
     # Find user with correct provider and uid
-    user = User.where(provider: auth.provider, uid: auth.uid).first
+    user = User.where(email: auth.info.email).first
 
     # Didn't find any user so create one
     unless user
-      user = User.new(name: auth.info.name, email: (auth.info.email or "#{auth.info.nickname}@fake-twitter-email.com"), 
+      user = User.create(name: auth.info.name, email: (auth.info.email or "#{auth.info.nickname}@fake-twitter-email.com"), 
                         provider: auth.provider, uid: auth.uid, password: Devise.friendly_token[0,20])
-      user.save(validate: false)
     end
 
     return user
